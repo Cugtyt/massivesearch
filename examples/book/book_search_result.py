9905101cbf8388f24eff5108d6b2cbdcba824a19
Cugtyt <cugtyt@qq.com>
2025-04-14 17:30:13 +0800

    refactor: simplify BookSearchResult initialization by using dataclass

--- examples/book/book_search_result.py
+++ examples/book/book_search_result.py
@@ -1,14 +1,14 @@
 """Book search result."""  # noqa: INP001
 
+from dataclasses import dataclass
+
 import pandas as pd
 
 from supersearch.spec.base_search_engine import BaseSearchResult
 
 
+@dataclass
 class BookSearchResult(BaseSearchResult):
     """Result of book text search."""
 
-    def __init__(self, result: pd.DataFrame) -> None:
-        """Initialize book search result."""
-        super().__init__()
-        self.result = result
+    result: pd.DataFrame
